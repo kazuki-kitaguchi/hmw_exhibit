88a84e08fad0dff46e805b1269689fae45465892
kazuki-kitaguchi <higeohogehoge@gmail.com>
2020-03-08 07:18:36 +0900

    ユーザー名によるログイン機能を実装し、ユーザー情報を現在のパスワードを入力しなくても変更できるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :trackable and :omniauthable
+  attr_accessor :login
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable, :timeoutable
+         :recoverable, :rememberable, :validatable, :confirmable, :timeoutable, :authentication_keys => [:login]
+
+  validates :username,
+  length: { minimum: 4, maximum: 20 },
+  format: { with: /\A[a-z0-9]+\z/, message: "ユーザー名は半角英数字です"}
+  
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'   
   } 
+  resources :users, :only => [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
