f95e3349aefbd692d937819d78c0eae13847ee2e
kazuki-kitaguchi <higeohogehoge@gmail.com>
2020-03-09 22:34:50 +0900

    開発環境での画像アップロード先を管理外へ設定を反映

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  root 'home#index'
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
-  } 
-  resources :users, :only => [:show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
